bcd4c5be919267765b8c5feff82e406b11d2eefc
MayaMonikaa <mmonikaadenise@outlook.com>
2021-07-11 21:32:40 -0400

    Removed quotation marks from both sets of console.log parenthesis

--- module4-solution/harder/SpeakGoodBye.js
+++ module4-solution/harder/SpeakGoodBye.js
@@ -19,7 +19,7 @@
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
 byeSpeaker.speakWord=function(name) {
-  console.log(speakWord + " " + name);
+  console.log(speakWord + name);
 }
 
 window.byeSpeaker=byeSpeaker;
