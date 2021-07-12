cf2d3a294c92f6d609ffadc70ce47a50cc7ef826
MayaMonikaa <mmonikaadenise@outlook.com>
2021-07-11 22:57:22 -0400

    Changed name to names on hellospeak and byespeak

--- module4-solution/harder/SpeakGoodBye.js
+++ module4-solution/harder/SpeakGoodBye.js
@@ -18,8 +18,8 @@
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-byeSpeaker.speakWord=function(name) {
-  console.log(speakWord + name);
+byeSpeaker.speakWord=function(names) {
+  console.log(speakWord + names);
 }
 
 window.byeSpeaker=byeSpeaker;
