15e58883476641a8494a7ba395c2657d2b7b3d6b
MayaMonikaa <mmonikaadenise@outlook.com>
2021-07-11 22:28:37 -0400

    Added window function to script.js

--- module4-solution/harder/script.js
+++ module4-solution/harder/script.js
@@ -34,18 +34,16 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 (function (window) {
-  var yaakovGreeter ={};
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-  console.log(names)
-
-
+  console.log(names);
 
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using either the helloSpeaker's or byeSpeaker's 'speak' method.
 // See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */) {
-
+for (var i=0; i<names.length; i++) {
+  console.log(helloSpeak+names[i]);
+};
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
   // Use the string object's 'charAt' function. Since we are looking for
@@ -60,9 +58,13 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (/* fill in condition here */) {
+  if () {
+    byespeaker.names
     // byeSpeaker.xxxx
-  } else {
+  } 
+  else {
+    hellospeaker.names
     // helloSpeaker.xxxx
   }
-}
+  window.names=names
+})(window)
