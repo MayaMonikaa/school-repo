0cabebe85e701e8df573a980360cccdafbf0a4cb
MayaMonikaa <mmonikaadenise@outlook.com>
2021-07-11 23:14:02 -0400

    Edited remaining errors

--- module4-solution/harder/script.js
+++ module4-solution/harder/script.js
@@ -33,15 +33,16 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-(function (window) {
+(function () {
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-  console.log(names);
 
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using either the helloSpeaker's or byeSpeaker's 'speak' method.
 // See Lecture 50, part 1
 for (var i=0; i<names.length; i++) {
+  var firstLetter=names[i].charAt(0).toLowerCase();
+
   console.log(helloSpeak+names[i]);
 };
   // STEP 11:
@@ -58,13 +59,13 @@ for (var i=0; i<names.length; i++) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if () {
-    byespeaker.names
+  if (firstLetter === 'j') {
+    byeSpeaker(names[i])
     // byeSpeaker.xxxx
   } 
   else {
-    hellospeaker.names
+    helloSpeaker(names[i])
     // helloSpeaker.xxxx
   }
-  window.names=names
-})(window)
+  
+})()
