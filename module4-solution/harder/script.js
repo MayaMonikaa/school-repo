4e5d6de743a164aa0cc81adcd4de67dee2af4767
MayaMonikaa <mmonikaadenise@outlook.com>
2021-07-11 23:26:55 -0400

    Space additions

--- module4-solution/harder/script.js
+++ module4-solution/harder/script.js
@@ -34,13 +34,14 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 (function () {
+  
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using either the helloSpeaker's or byeSpeaker's 'speak' method.
 // See Lecture 50, part 1
-for (var i=0; i<names.length; i++) {
+  for (var i=0; i<names.length; i++) {
   var firstLetter=names[i].charAt(0).toLowerCase();
 
   // STEP 11:
